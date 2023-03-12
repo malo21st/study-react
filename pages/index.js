344fd7b60e967810837c34ff100ed349cadcea31
malo21st <44739330+malo21st@users.noreply.github.com>
2023-03-12 17:39:56 +0900

    💬 H1タイトル修正

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            malo21st get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+          <h1>
+            malo21st page
+          </h1>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
